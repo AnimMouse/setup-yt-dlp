d01399afdce49ae7e9d09730d8201069a75437eb
AnimMouse <git@animmouse.com>
2022-02-26 15:09:45 +0800

    Include ffprobe

--- scripts/Download-yt-dlp-FFmpeg-Linux.sh
+++ scripts/Download-yt-dlp-FFmpeg-Linux.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo ::group::Downloading latest yt-dlp/FFmpeg-Builds for Linux
 aria2c -x 16 https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
-tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz --strip-components 2 --no-anchored ffmpeg
+tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz --strip-components 2 --no-anchored ffmpeg ffprobe
 mkdir yt-dlp-ffmpeg
-mv ffmpeg ./yt-dlp-ffmpeg/
+mv ffmpeg ffprobe ./yt-dlp-ffmpeg/
 rm ffmpeg-master-latest-linux64-gpl.tar.xz
 echo ::endgroup::
