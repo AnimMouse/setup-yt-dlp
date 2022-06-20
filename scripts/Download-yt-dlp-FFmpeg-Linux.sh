50e214ed2d3879ede90a802fd571226fad4b4555
AnimMouse <git@animmouse.com>
2022-06-20 16:42:06 +0800

    Fail script when error and update upload-artifact to v3

--- scripts/Download-yt-dlp-FFmpeg-Linux.sh
+++ scripts/Download-yt-dlp-FFmpeg-Linux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 echo ::group::Downloading latest yt-dlp/FFmpeg-Builds for Linux
 aria2c -x 16 https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz
 tar -xf ffmpeg-master-latest-linux64-gpl.tar.xz --strip-components 2 --no-anchored ffmpeg ffprobe
--- scripts/Download-yt-dlp-FFmpeg-Windows.ps1
+++ scripts/Download-yt-dlp-FFmpeg-Windows.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = 'Stop'
 Write-Host ::group::Downloading latest yt-dlp/FFmpeg-Builds for Windows
 aria2c -x 16 https://github.com/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl.zip
 7z e ffmpeg-master-latest-win64-gpl.zip ffmpeg.exe ffprobe.exe -oyt-dlp-ffmpeg -r
