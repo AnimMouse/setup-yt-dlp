588bd184c85dec3aeeaaa801b399166e2ecc252b
Anim Mouse <git@animmouse.com>
2023-10-27 16:21:53 +0800

    feat: Error when attempting to install FFmpeg on macOS

--- scripts/yt-dlp-ffmpeg/Linux.sh
+++ scripts/yt-dlp-ffmpeg/Linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 echo ::group::Downloading latest yt-dlp/FFmpeg-Builds for Linux
+if [ $RUNNER_OS = macOS ]; then echo ::error::yt-dlp/FFmpeg-Builds currently does not have macOS builds, please use AnimMouse/setup-ffmpeg instead.; fi
 mkdir -p yt-dlp-FFmpeg
 wget -qO- $GITHUB_SERVER_URL/yt-dlp/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-linux64-gpl.tar.xz | tar -xJC yt-dlp-FFmpeg --strip-components 2 --no-anchored ffmpeg ffprobe
 echo ::endgroup::
