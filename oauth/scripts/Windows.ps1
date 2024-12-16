30d5c9b2f9efd6c53210ad426566678e414e1dba
Anim Mouse <git@animmouse.com>
2024-12-16 22:48:35 +0800

    feat: Remove OAuth action

--- oauth/scripts/Windows.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Set-StrictMode -Version Latest
-Write-Host ::group::Signing in to yt-dlp YouTube OAuth
-New-Item $env:USERPROFILE\.cache\yt-dlp\youtube -ItemType Directory -Force
-$refresh_token = [Convert]::FromBase64String($env:refresh_token)
-[IO.File]::WriteAllBytes("$env:USERPROFILE\.cache\yt-dlp\youtube\oauth_refresh_token_default.json", $refresh_token)
-Write-Host ::endgroup::
